4a4d3f1d2b0db69b8b6f8940089f3e50eb53dda8
Aergia <dook@aergia.dk>
2017-09-10 19:18:12 +0200

    Basic mesh deformation test and reference pictures.

---
+++ Assets/Scripts/ClickDeform.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class ClickDeform : MonoBehaviour {
+
+    //private MeshFilter meshFilter;
+
+    //Mesh deformingMesh;
+    //Vector3[] originalVertices, displacedVertices;
+
+    //float uniformScale = 1f;
+
+    //void Start ()
+    //{
+    //    meshFilter = GetComponent<MeshFilter>();
+
+    //    deformingMesh = GetComponent<MeshFilter>().mesh;
+    //    originalVertices = deformingMesh.vertices;
+    //    displacedVertices = new Vector3[originalVertices.Length];
+    //    for (int i = 0; i < originalVertices.Length; i++)
+    //    {
+    //        displacedVertices[i] = originalVertices[i];
+    //    }
+    //}
+
+    //void Update ()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        HandleInput();
+    //    }
+
+    //    uniformScale = transform.localScale.x;
+    //    for (int i = 0; i < displacedVertices.Length; i++)
+    //    {
+    //        UpdateVertex(i);
+    //    }
+    //    deformingMesh.vertices = displacedVertices;
+    //    deformingMesh.RecalculateNormals();
+    //}
+
+    //void HandleInput()
+    //{
+    //    Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    RaycastHit hit;
+
+    //    if (Physics.Raycast(inputRay, out hit))
+    //    {
+    //        if(hit.collider.gameObject == this)
+    //        {
+    //            Vector3 point = hit.point;
+    //            point += hit.normal * forceOffset;
+    //            deformer.AddDeformingForce(point, force);
+    //        }
+    //    }
+    //}
+
+
+
+}
---
+++ Assets/Scripts/MeshEater.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshEater : MonoBehaviour {
+
+    public float springForce = 20f;
+    public float damping = 5f;
+
+    Mesh deformingMesh;
+    Vector3[] originalVertices, displacedVertices;
+
+    MeshCollider meshCollider;
+
+    HashSet<int> eatenVerts = new HashSet<int>();
+
+    float uniformScale = 1f;
+
+    void Start()
+    {
+        meshCollider = GetComponent<MeshCollider>();
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+        originalVertices = deformingMesh.vertices;
+        displacedVertices = new Vector3[originalVertices.Length];
+        for (int i = 0; i < originalVertices.Length; i++)
+        {
+            displacedVertices[i] = originalVertices[i];
+        }
+    }
+
+    void Update()
+    {
+        //uniformScale = transform.localScale.x;
+        //for (int i = 0; i < displacedVertices.Length; i++)
+        //{
+        //    UpdateVertex(i);
+        //}
+
+        //deformingMesh.vertices = displacedVertices;
+        //deformingMesh.RecalculateNormals();
+
+        //meshCollider.sharedMesh = null;
+        //meshCollider.sharedMesh = deformingMesh;
+    }
+
+    public void EatVertex(Vector3 point, Vector3 normal)
+    {
+        point = transform.InverseTransformPoint(point);
+        int eatVertex = GetNearestVertex(point);
+
+        if (eatenVerts.Contains(eatVertex)) return;
+
+        normal = deformingMesh.normals[eatVertex] * 0.002f;
+
+        for (int i = 1; i < displacedVertices.Length; i++)
+        {
+            if(displacedVertices[i] == displacedVertices[eatVertex])
+            {
+                displacedVertices[i] = displacedVertices[i] - normal;
+                eatenVerts.Add(i);
+            }
+        }
+
+        //displacedVertices[eatVertex] = displacedVertices[eatVertex] - normal;
+
+        deformingMesh.vertices = displacedVertices;
+        deformingMesh.RecalculateNormals();
+        deformingMesh.RecalculateTangents();
+
+        meshCollider.sharedMesh = null;
+        meshCollider.sharedMesh = deformingMesh;
+
+        eatenVerts.Add(eatVertex);
+
+        //for (int i = 0; i < displacedVertices.Length; i++)
+        //{
+
+        //}
+    }
+
+    int GetNearestVertex(Vector3 point)
+    {
+        int result = 0;
+        float smallestDist = Vector3.Distance(point, displacedVertices[0]);
+        for (int i = 1; i < displacedVertices.Length; i++)
+        {
+            float d = Vector3.Distance(point, displacedVertices[i]);
+            if(d < smallestDist)
+            {
+                smallestDist = d;
+                result = i;
+            }
+        }
+        return result;
+    }
+
+}
